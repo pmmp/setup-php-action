c90904e57e5c15747da2d1b070b077c2e0fc1de3
Dylan T <dktapps@pmmp.io>
2021-11-07 21:47:05 +0000

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,42 @@
-import * as core from '@actions/core'
 import * as cache from '@actions/cache'
+import * as core from '@actions/core'
+import * as path from 'path'
 import {exec} from '@actions/exec'
 import {hashFiles} from '@actions/glob'
-import * as path from 'path'
 
 async function run(): Promise<void> {
-    try {
-        const phpVersion: string = core.getInput('php-version')
-        const installPath: string = core.getInput('install-path')
+  try {
+    const phpVersion: string = core.getInput('php-version')
+    const installPath: string = core.getInput('install-path')
 
-        const scriptsPath: string = path.dirname(__dirname)
-        const buildShPath: string = scriptsPath + path.sep + 'build.sh'
-        const primaryCacheKey: string = 'php-build-generic-' + phpVersion + '-' + hashFiles(buildShPath)
+    const scriptsPath: string = path.dirname(__dirname)
+    const buildShPath: string = path.join(scriptsPath, 'build.sh')
+    const primaryCacheKey = `php-build-generic-${phpVersion}-${hashFiles(
+      buildShPath
+    )}`
 
-        const hitCacheKey: string|undefined = await cache.restoreCache([installPath], primaryCacheKey)
+    const hitCacheKey: string | undefined = await cache.restoreCache(
+      [installPath],
+      primaryCacheKey
+    )
 
-        if (hitCacheKey == undefined) {
-            core.info('Compiling new binaries: PHP ' + phpVersion + ' to be installed in ' + installPath)
-            await exec(buildShPath, [phpVersion, installPath])
-            core.info('Storing cache')
-            await cache.saveCache([installPath], primaryCacheKey)
-        } else {
-            core.info('Installing dependencies for cached PHP build')
-            await exec(scriptsPath + path.sep + 'install-dependencies.sh')
-        }
-        core.info('Adding PHP to PATH')
-        core.addPath(installPath)
-        core.info('Done!')
-    } catch (error) {
-        if (error instanceof Error) core.setFailed(error.message)
+    if (hitCacheKey === undefined) {
+      core.info(
+        `Compiling new binaries: PHP ${phpVersion} to be installed in ${installPath}`
+      )
+      await exec(buildShPath, [phpVersion, installPath])
+      core.info('Storing cache')
+      await cache.saveCache([installPath], primaryCacheKey)
+    } else {
+      core.info('Installing dependencies for cached PHP build')
+      await exec(path.join(scriptsPath, 'install-dependencies.sh'))
     }
+    core.info('Adding PHP to PATH')
+    core.addPath(installPath)
+    core.info('Done!')
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }
 
 run()
