cd773092218dd46954004f7b40a9b1a0601b3546
Dylan T <dktapps@pmmp.io>
2021-11-07 21:35:36 +0000

    First shot at ... anything?

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,35 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as cache from '@actions/cache'
+import {exec} from '@actions/exec'
+import {hashFiles} from '@actions/glob'
+import * as path from 'path'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    try {
+        const phpVersion: string = core.getInput('php-version')
+        const installPath: string = core.getInput('install-path')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        const scriptsPath: string = path.dirname(__dirname)
+        const buildShPath: string = scriptsPath + path.sep + 'build.sh'
+        const primaryCacheKey: string = 'php-build-generic-' + phpVersion + '-' + hashFiles(buildShPath)
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+        const hitCacheKey: string|undefined = await cache.restoreCache([installPath], primaryCacheKey)
+
+        if (hitCacheKey == undefined) {
+            core.info('Compiling new binaries: PHP ' + phpVersion + ' to be installed in ' + installPath)
+            await exec(buildShPath, [phpVersion, installPath])
+            core.info('Storing cache')
+            await cache.saveCache([installPath], primaryCacheKey)
+        } else {
+            core.info('Installing dependencies for cached PHP build')
+            await exec(scriptsPath + path.sep + 'install-dependencies.sh')
+        }
+        core.info('Adding PHP to PATH')
+        core.addPath(installPath)
+        core.info('Done!')
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message)
+    }
 }
 
 run()
