ea71138ee1f044a5e037253cca8bf3d3a5ccb5e2
Dylan K. Taylor <dktapps@pmmp.io>
2023-05-31 18:44:21 +0000

    Use binaries from pmmp/PHP-Binaries, install Composer automatically, massively simplified action
    the action now accepts only a major.minor (e.g. 8.1, 8.2) and installs
    binaries directly from pmmp/PHP-Binaries.
    
    This is not only substantially simpler, but much faster (the binaries
    themselves are also faster), and significantly reduces complexity.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import * as cache from '@actions/cache'
 import * as core from '@actions/core'
 import * as path from 'path'
-import {exec} from '@actions/exec'
-import * as crypto from 'crypto'
-import {promises as fsPromises} from 'fs'
+import * as toolCache from '@actions/tool-cache'
+import {promises as fs} from 'fs'
 
 async function run(): Promise<void> {
   try {
@@ -12,39 +10,33 @@ async function run(): Promise<void> {
     const absoluteInstallPath: string = path.resolve(installPath)
     const pmVersionMajor: string = core.getInput('pm-version-major')
 
-    const scriptsPath: string = path.dirname(__dirname)
-    const buildShPath: string = path.join(scriptsPath, 'build.sh')
-    const buildShContents: string = await fsPromises.readFile(
-      buildShPath,
-      'utf8'
+    //TODO: MacOS and Windows support
+    const binaryTarballUrl = `https://github.com/pmmp/PHP-Binaries/releases/download/php-${phpVersion}-latest/PHP-Linux-x86_64-PM${pmVersionMajor}.tar.gz`
+    core.info(`Downloading PHP binary: ${binaryTarballUrl}`)
+    const binaryTarballPath: string = await toolCache.downloadTool(
+      binaryTarballUrl
     )
-    const buildShHash: string = crypto
-      .createHash('sha256')
-      .update(buildShContents)
-      .digest('hex')
-
-    const primaryCacheKey = `php-build-generic-${phpVersion}-${buildShHash}-pm-${pmVersionMajor}`
-
-    core.info(`Looking for cached binaries under key ${primaryCacheKey}`)
-
-    const hitCacheKey: string | undefined = await cache.restoreCache(
-      [absoluteInstallPath],
-      primaryCacheKey
+    const binaryDir: string = await toolCache.extractTar(
+      binaryTarballPath,
+      absoluteInstallPath
     )
+    const composerUrl =
+      'https://getcomposer.org/download/latest-2.x/composer.phar'
+    core.info(`Downloading latest Composer 2.x version: ${composerUrl}`)
+    const composerDownloadPath: string = await toolCache.downloadTool(
+      composerUrl
+    )
+    const executablePath: string = path.join(
+      absoluteInstallPath,
+      'bin/php7/bin'
+    )
+    const composerExecutablePath: string = path.join(executablePath, 'composer')
+    await fs.copyFile(composerDownloadPath, composerExecutablePath)
+    await fs.chmod(composerExecutablePath, 0o755)
 
-    if (hitCacheKey === undefined) {
-      core.info(
-        `Compiling new binaries: PHP ${phpVersion} for PocketMine-MP ${pmVersionMajor} to be installed in ${absoluteInstallPath}`
-      )
-      await exec(buildShPath, [phpVersion, absoluteInstallPath, pmVersionMajor])
-      core.info('Storing cache')
-      await cache.saveCache([absoluteInstallPath], primaryCacheKey)
-    } else {
-      core.info('Installing dependencies for cached PHP build')
-      await exec(path.join(scriptsPath, 'install-dependencies.sh'))
-    }
-    core.info('Adding PHP to PATH')
-    core.addPath(path.join(absoluteInstallPath, 'bin'))
+    core.info('Adding PHP and Composer to PATH')
+    core.addPath(path.join(binaryDir, 'bin/php7/bin'))
+
     core.info('Done!')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
